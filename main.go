dca698d7a9e64ca701b8b16e561b595fc02160bd
ccd <ccd1020@yeah.net>
2022-08-25 19:07:56 +0800

    支持Windows字体

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"os"
-	// "fmt"
+	"runtime"
+	"fmt"
 	"strings"
 	"gui/master"
 	findfont "github.com/flopp/go-findfont"
 	
 )
 
-func init(){
-	fontPaths := findfont.List()
-	for _, path := range fontPaths {
-		// fmt.Println(path)
-		//楷体:simkai.ttf
-		//黑体:simhei.ttf
-		if strings.Contains(path, "STHeiti Medium.ttc") {
-			os.Setenv("FYNE_FONT", path)
-			break
-		}
-	}
+func init() {
+    sysType := runtime.GOOS
+    fontPaths := findfont.List()
+    for _, path := range fontPaths {
+        // fmt.Println(path)
+        //楷体:simkai.ttf
+        //黑体:simhei.ttf
+
+        switch sysType {
+        case "windows":
+            if strings.Contains(path, "simkai.ttf") {
+                os.Setenv("FYNE_FONT", path)
+
+            }
+        case "darwin":
+            if strings.Contains(path, "STHeiti Medium.ttc") {
+                os.Setenv("FYNE_FONT", path)
+
+            }
+        }
+
+    }
+    fmt.Println(sysType)
 }
 
 func main(){
